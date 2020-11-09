62e121ca79f1d56eac5cf8ca1dcf96f5b81a89a9
Soren Nissen <s.nissen.bass@gmail.com>
2020-11-09 21:43:33 +0000

    exercise 2 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,13 +3,7 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
 
-
-# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
-#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
-#   * Gastown (annual_revenue of 190000 carries men's apparel only)
   
 
 store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
@@ -17,4 +11,4 @@ store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: fa
 store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 
-
+puts Store.count(:name)
